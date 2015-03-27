a1c918779afa5e92843c937e14218f66c1b9f4ae
Christopher Collins <chris@spring215.com>
2015-03-27 17:45:30 -0400

    init push

---
+++ js/js.js
@@ -0,0 +1,6 @@
+function initRoot()
+{
+	document.getElementById('styleSheetRoot').rel = 'stylesheet';
+	document.getElementById('styleSheetRoot').type = 'text/css';
+	document.getElementById('styleSheetRoot').href = 'css/css.css';
+}
